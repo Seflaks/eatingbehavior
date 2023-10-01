4ae15d4397aabaf2cd2f6b2bad458705f1d71ddb
seflaks <spk.lotos@gmail.com>
2023-10-01 22:09:43 +0300

    implements filter meals by time with cycles

--- src/main/java/ru/makkin/eatingbehavior/util/UserMealsUtil.java
+++ src/main/java/ru/makkin/eatingbehavior/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.makkin.eatingbehavior.util;
 import ru.makkin.eatingbehavior.model.UserMeal;
 import ru.makkin.eatingbehavior.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -29,7 +29,25 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
-        return null;
+        List<UserMealWithExcess> result = new ArrayList<>();
+        Map<LocalDate, Integer> mapCaloriesPerDay = new HashMap<>();
+        for (UserMeal meal : meals) {
+            LocalDate mealDate = meal.getDateTime().toLocalDate();
+            System.out.println(mealDate);
+            System.out.println(meal);
+//            mapCaloriesPerDay.put(mealDate,  meal.getCalories() + mapCaloriesPerDay.getOrDefault(mealDate, 0));
+            mapCaloriesPerDay.merge(mealDate, meal.getCalories(), (old, currentCalories) -> old + currentCalories);
+            System.out.println(mapCaloriesPerDay.get(mealDate));
+        }
+        mapCaloriesPerDay.forEach((a, b) -> System.out.println(a + " " + b));
+        for (UserMeal meal : meals) {
+            if(!TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                continue;
+            }
+            result.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
+                    mapCaloriesPerDay.get(meal.getDateTime().toLocalDate()) > caloriesPerDay ? false : true));
+        }
+        return result;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
